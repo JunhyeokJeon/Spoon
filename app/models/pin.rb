e7c2b3e1f6cd3c2c9e30d4659fdabd5c92aa4128
jeon junhyeok <inamorfatis@gmail.com>
2018-05-23 12:59:55 +0900

    Add image uploading with PaperClip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,6 @@
 class Pin < ApplicationRecord
   belongs_to :user
+
+  has_attached_file :image, styles: { medium: "300x300>", big: "900x900>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
